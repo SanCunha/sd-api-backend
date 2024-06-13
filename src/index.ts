061ebdaf7080cf08ed9590045f76701c6023e5b0
San Cunha <san.silva@aluno.ufop.edu.br>
2024-06-13 13:55:40 -0300

    First version Azure

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
-import express, { Request, Response } from 'express';
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import jwt from 'jsonwebtoken';
-import bodyParser from 'body-parser';
 
-const app = express();
-const port = 3000;
 const secretKey = '29619b621392f4aae0e7eb8dc83ae746';
 
-app.use(bodyParser.json());
-
+// Sample user data
 const users: { [key: string]: string } = {
-  'kaka': '152computacao',
-  'frodo': '121civil'
+    'kaka': '152computacao',
+    'frodo': '121civil'
+  };
+
+interface LoginRequestBody {
+    username: string;
+    password: string;
+}
+
+export async function loginHandler(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const body: LoginRequestBody = await request.json() as LoginRequestBody;
+    const { username, password } = body;
+
+
+    if (users[username] && users[username] === password) {
+
+        const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+        return {
+            status: 200,
+            jsonBody: { token }
+        };
+    } else {
+
+        return {
+            status: 401,
+            jsonBody: { message: 'Invalid credentials' }
+        };
+    }
 };
 
-app.post('/login', (req: Request, res: Response) => {
-  const { username, password } = req.body;
 
-  if (users[username] && users[username] === password) {
-    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
-  }
-
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.http('loginHandler', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: loginHandler
 });
